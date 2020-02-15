ac431a798db3887d85ac865ce0c684c7f5dd4b78
= <=>
2020-02-15 18:25:50 +0300

    chechBox function complited

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -14,12 +14,12 @@ class Sudoku extends React.Component {
         [0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,],
-        [0,0,0,0,0,0,0,0,0,],]
+        [0,0,0,0,0,0,0,0,0,],
+      ]
     };
   }
 
   componentDidMount(){
-    
     this.fillMySudoku()
   }
 
@@ -29,17 +29,20 @@ class Sudoku extends React.Component {
       for (let j = 0; j < 9; j++) {
          do{
           var randomNumber = Math.floor(Math.random()*10)
-   
-          if(this.checkRow(i,randomNumber,sudoku)&&this.checkColumn(j,randomNumber, sudoku)){
+          
+          if(
+            this.checkRow(i, randomNumber, sudoku) && 
+            this.checkColumn(j, randomNumber, sudoku) &&
+            this.checkBox(i, j, randomNumber, sudoku)
+            ){
             sudoku[i][j]=randomNumber
           }else{
-            sudoku[i][j]=''      
+            sudoku[i][j]=null    
           } 
-        }while (sudoku[i][j] === 0)
+        }while (sudoku[i][j] === 0 )
       } 
     }
-    console.log(sudoku);
-    
+    console.log(sudoku);    
   }
 
   checkRow = (i,randomNumber, array) => {  
@@ -52,12 +55,55 @@ class Sudoku extends React.Component {
   }
 
   checkColumn = (j,randomNumber, array) => {  
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {      
       if(array[i][j]===randomNumber) {
         return false
       }   
     }
     return true
+  }
+
+  checkBox = (i, j, randomNumber, array) => {
+    var object = this.takeNumberPosition(i,j)    
+    for (let r = object.x1; r <= object.x2; r++) {
+      for (let c = object.y1; c <= object.y2; c++) {
+        if(array[r][c]===randomNumber) 
+          return false
+      }
+    }
+    return true
+  }
+
+  takeNumberPosition = (i,j) => {
+    var object = {
+      x1:0,
+      x2:0,
+      y1:0,
+      y2:0
+    }
+
+    if (i>=0 && i<3) {
+      object.x1=0
+      object.x2=2
+    }else if (i>=3 && i<6) {
+      object.x1=3
+      object.x2=5
+    }else if (i>=6 && i<9) {
+      object.x1=6
+      object.x2=8
+    }
+
+    if (j>=0 && j<3) {
+      object.y1=0
+      object.y2=2
+    }else if (j>=3 && j<6) {
+      object.y1=3
+      object.y2=5
+    }else if (j>=6 && j<9) {
+      object.y1=6
+      object.y2=8
+    }
+    return object
   }
 
   render() {
